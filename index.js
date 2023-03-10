7585eb9e37359b9fa398fc8d1ee0ac28c3ea8dcd
DhanushM12 <dhanushmukhi@gmail.com>
2023-03-10 20:54:29 +0530

    Added jwt token generation

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const jwt = require('jsonwebtoken')
 
 app.get('/', (req, res) => {
     res.json({message: "welcome to node js"})
     // res.send('<h1>Welcome to node js!!!</h1>')
 })
 
+app.post('/tokenGenerate', (req, res) => {
+    const user = {
+        id: 12345,
+        username: 'AugFreshers22',
+        email: 'augfresh22@coding.com'
+    }
+    jwt.sign(user, 'secretkey', function(err, token) {
+        if(err){
+            res.sendStatus(403)
+        }
+        else{
+            res.json({
+                token
+            })
+        }
+      });
+      
+})
 
 app.listen(port, function(err){
     if(err){
